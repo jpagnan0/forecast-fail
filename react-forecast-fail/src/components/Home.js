3c6ae0be4256d036619680bb6e61fb28062ef062
jpagnan0 <a.jpagnano@gmail.com>
2019-02-12 11:10:24 -0500

    link from button

--- react-forecast-fail/src/components/Home.js
+++ react-forecast-fail/src/components/Home.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import {Link} from 'react-router-dom'
 // RAILSAPI = 'http://localhost:3000/api/v1/users/'
 
 export default class Home extends Component {
@@ -10,53 +10,68 @@ export default class Home extends Component {
     super(props);
 
     this.state = {
-      usernameInput: '',
+      usernameInput: "",
       listOfusers: []
-    }
+    };
   }
 
-  postApiCreateUser = (event) => {
-    console.log('state', this.state.usernameInput);
-    event.preventDefault()
-    fetch('http://localhost:3000/api/v1/users/', {
-      method: 'POST',
+  postApiCreateUser = event => {
+    console.log("state", this.state.usernameInput);
+    event.preventDefault();
+    fetch("http://localhost:4000/api/v1/users/", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        "Content-Type": "application/json",
+        Accept: "application/json"
       },
       body: JSON.stringify({
         username: this.state.usernameInput
       })
-    }).then(console.log)
+    }).then(console.log);
     // debugger
-      document.querySelector('#userForm').reset()
-  }
+    document.querySelector("#userForm").reset();
+  };
 
   // componentDidMount() {
   //   this.postFetchUserApi()
   // }
 
-  handleChangeUsername = (e) => {
+  handleChangeUsername = e => {
     // console.log(e.target.value);
     this.setState({
       [e.target.id]: e.target.value
-    })
-  }
-
+    });
+  };
 
   render() {
     return (
-      <div>
-        <div>Home</div>
-        <form id="userForm" onSubmit={this.postApiCreateUser}>
-          <label htmlFor="username">User Name: </label>
-          <input onChange={this.handleChangeUsername} id="usernameInput" type="text" name="name"/>
-          <input type="submit" value="Log In"/>
-        </form>
-      </div>
+      <section className="hero is-primary is-fullheight">
+        <div className="hero-body">
+          <div className="container">
+            <div className="columns is-centered">
+              <div className="column">
+                <div className="column is-6-tablet is-12-desktop is-12-widescreen">
+                  <form className="box" id="userForm" onSubmit={this.postApiCreateUser}>
+                    <div className="field">
+                      <label htmlFor="username">User Name: </label>
+                      <input
+                        className="input"
+                        onChange={this.handleChangeUsername}
+                        id="usernameInput"
+                        type="text"
+                        name="name"
+                      />
+                      <button className="button is-success" type="submit" value="Log In"><Link to="/search">Log in</Link></button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
-
 
   // <Router>
   //   <div className="App">
